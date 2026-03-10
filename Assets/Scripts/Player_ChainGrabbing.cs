ae390f37a5f51825204a3f3067251aeee9a4c6f6
ACookedGuy <iyertanish@gmail.com>
2026-03-10 16:57:06 +0530

    update to chain grabbing

--- Assets/Scripts/Player_ChainGrabbing.cs
+++ Assets/Scripts/Player_ChainGrabbing.cs
@@ -1,39 +1,47 @@
 using UnityEngine;
 
-public class Player_ChainGrabbing: MonoBehaviour
+public class Player_ChainGrabbing : MonoBehaviour
 {
     private HingeJoint2D joint;
     private Rigidbody2D rb;
 
     private bool grabbing;
 
+    public KeyCode swingKey = KeyCode.X;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
         joint = gameObject.AddComponent<HingeJoint2D>();
         joint.enabled = false;
+        joint.autoConfigureConnectedAnchor = false;
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Chain") && !grabbing)
+        if (collision.gameObject.CompareTag("SwingPoint") && !grabbing)
         {
-            Rigidbody2D chainRB = collision.gameObject.GetComponent<Rigidbody2D>();
-
-            if (chainRB != null)
+            if (Input.GetKey(swingKey))
             {
-                joint.connectedBody = chainRB;
-                joint.enabled = true;
+                Rigidbody2D swingRB = collision.gameObject.GetComponent<Rigidbody2D>();
 
-                grabbing = true;
-                rb.linearVelocity = Vector2.zero;
+                if (swingRB != null)
+                {
+                    joint.connectedBody = swingRB;
+                    joint.enabled = true;
+
+                    grabbing = true;
+
+                    rb.linearVelocity = Vector2.zero;
+                }
             }
         }
     }
 
     void Update()
     {
-        if (grabbing && Input.GetKeyDown(KeyCode.Space))
+        if (grabbing && !Input.GetKey(swingKey))
         {
             ReleaseChain();
         }
@@ -41,7 +49,6 @@ public class Player_ChainGrabbing: MonoBehaviour
 
     void ReleaseChain()
     {
-        rb.AddForce(new Vector2(0, 9f),ForceMode2D.Impulse);
         joint.enabled = false;
         joint.connectedBody = null;
         grabbing = false;
